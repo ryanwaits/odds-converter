3453f9449e5b03a05fe04085e7180d65cd9c820a
Ryan Waits <ryan.waits@gmail.com>
2016-03-19 14:33:21 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+## Odds Converter
+
+`npm install odds-converter`
+
+` var odds = require('odds-converter')`
+
+### Decimal to American
+
+Example:
+
+`odds.decimal.toAmerican(2.25); // 125.00`
+
+`odds.decimal.toAmerican(1.90); // -111.11`
+
+### Decimal to Fraction
+
+`// coming shortly`
+
+### Fraction to American
+
+`odds.fraction.toAmerican(7,4); // 175`
+
+`odds.fraction.toAmerican(5,6); // -120`
+
+### Fraction to Decimal
+
+`odds.fraction.toDecimal(3,2); // 3.2`
+
+`odds.fraction.toDecimal(1,5); // 1.2`
+
+### American to Decimal
+
+`odds.american.toDecimal(225); // 3.25`
+
+`odds.american.toDecimal(-110); // 1.91`
+
+### American to Fraction
+
+`odds.american.toFractional(225).simplify(); // (9/4)`
+
+`odds.american.toFractional(-125).simplify(); // (4/5)`
+
---
+++ index.js
@@ -0,0 +1,34 @@
+var Fraction = require('fractional-arithmetic').Fraction;
+var OddsConverter = OddsConverter || {};
+
+OddsConverter.decimal = {
+    toAmerican: function(decimal){
+        decimal < 2.0 ? moneyline = ( (-100)/(decimal - 1) ).toPrecision(5) : moneyline = ( (decimal - 1) * 100 ).toPrecision(5);
+        return moneyline;
+    },
+    toFraction: function(decimal){
+        // decimal to fraction
+    }
+}
+OddsConverter.fraction = {
+    toAmerican: function(n,d){
+        n > d ? moneyline = ((n/d) * 100) : moneyline = (-100)/(n/d);
+        return moneyline;
+    },
+    toDecimal: function(n,d){
+        return decimal = (n/d) + 1;
+    }
+}
+
+OddsConverter.american = {
+    toDecimal: function(moneyline){
+        moneyline > 0 ? decimal = (moneyline/100) + 1 : decimal = ((100/Math.abs(moneyline)) + 1).toPrecision(3);
+        return decimal;
+    },
+    toFractional: function(moneyline){
+        moneyline > 0 ? fraction = new Fraction(moneyline/100) : fraction = new Fraction(100/Math.abs(moneyline));
+        return fraction;
+    }
+}
+
+module.exports = OddsConverter;
